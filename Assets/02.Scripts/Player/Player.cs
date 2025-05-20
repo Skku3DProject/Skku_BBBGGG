4ffa4aa8489fd710b9a97bbe60ff20aff33675a1
minju8843 <minju8843@naver.com>
2025-05-20 14:26:15 +0900

    [05 - 20] 서민주 - 무기/도구 전환 기능 완료

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -3,78 +3,66 @@ using UnityEngine;
 using System.Collections.Generic;
 using Unity.Android.Gradle.Manifest;
 
+
 public class Player : MonoBehaviour
 {
     [Header("Player Stats")]
-    public PlayerStatsSO PlayerStats;//체력, 이동 속도
+    public PlayerStatsSO PlayerStats;
 
-    [Header("Weapon")]
-    public List<WeaponAttackSO> Weapons = new List<WeaponAttackSO>();
-    [SerializeField]
-    private int _currentWeaponIndex = 0;//현재 무기 인덱스 0이 검
-
-    [SerializeField]
-    private WeaponAttackSO _currnetWeapon;//현재 무기
-
-
-    private float _currentHealth;
     
-    private CharacterController _characterController;
+
+    //캐릭터 움직임 관련
+    private float _gravity = -9.81f;
+    private float _currentHealth;
     private Vector3 _velocity;
     private bool _isGrounded;
-    private float _gravity = -9.81f;
+    private CharacterController _characterController;
+    
 
     public Transform GroundCheck;
     public float GroundDistance = 0.4f;
     public LayerMask GroundMask;
 
+    // 애니메이션
+    private Animator _playerAnimator;
+
     private void Start()
     {
-
         _characterController = GetComponent<CharacterController>();
+        _playerAnimator = GetComponent<Animator>();
 
-        _currnetWeapon = Weapons[_currentWeaponIndex];
-        Debug.Log($"현재 무기 : {_currnetWeapon.WeaponName}");
-
+       
     }
-
 
     private void Update()
     {
-        //바닥 확인
+        // 바닥 확인
         _isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
 
-        if(_isGrounded && _velocity.y < 0)
+        if (_isGrounded && _velocity.y < 0)
         {
             _velocity.y = -2f;
         }
 
-        //입력
-        float x = Input.GetAxis("Horizontal");//좌/우
-        float z = Input.GetAxis("Vertical");//위/아래
-
+        // 이동 입력
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-
-        //이동
         _characterController.Move(move * PlayerStats.MoveSpeed * Time.deltaTime);
 
-        //점프
-        if(Input.GetButtonDown("Jump") && _isGrounded)
+        // 점프
+        if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            Debug.Log("땅에 닿았으니 점프가능");
             _velocity.y = Mathf.Sqrt(PlayerStats.JumpPower * -2f * _gravity);
         }
 
-
+        // 중력 적용
         _velocity.y += _gravity * Time.deltaTime;
         _characterController.Move(_velocity * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Debug.Log("Q키 눌림");
-            _currentWeaponIndex = (_currentWeaponIndex + 1) % Weapons.Count;
-            _currnetWeapon = Weapons[_currentWeaponIndex];
-            Debug.Log($"무기 변경: {_currnetWeapon.WeaponName}");
-        }
+        
     }
+
+    
+
 }
---
+++ Assets/02.Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public enum EquipmentType
+{
+    Sword,
+    Bow,
+    Magic,
+    Pickaxe
+}
+public class PlayerEquipmentController : MonoBehaviour
+{
+    [Header("장비 오브젝트")]
+    public GameObject Sword;
+    public GameObject Shield;
+    public GameObject Pickaxe;
+    public GameObject Bow;
+    public GameObject Magic;
+
+    [Header("애니메이터")]
+    public Animator animator;
+
+    // 현재 장비 타입
+    private EquipmentType _currentEquipType = EquipmentType.Sword;
+
+    // 장비 타입 배열 (순환용)
+    private EquipmentType[] weapons = { EquipmentType.Sword, EquipmentType.Bow, EquipmentType.Magic };
+    private EquipmentType tool = EquipmentType.Pickaxe;
+
+    void Start()
+    {
+        SetEquipment(_currentEquipType);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) // 무기 순환
+        {
+            CycleWeapons();
+        }
+        if (Input.GetKeyDown(KeyCode.E)) // 도구 활성화
+        {
+            SetEquipment(tool);
+        }
+    }
+
+    private void CycleWeapons()
+    {
+        int currentIndex = System.Array.IndexOf(weapons, _currentEquipType);
+        int nextIndex = (currentIndex + 1) % weapons.Length;
+        SetEquipment(weapons[nextIndex]);
+    }
+
+    private void SetEquipment(EquipmentType equipType)
+    {
+        _currentEquipType = equipType;
+
+        // 모두 비활성화
+        Sword.SetActive(false);
+        Shield.SetActive(false);
+        Pickaxe.SetActive(false);
+        Bow.SetActive(false);
+        Magic.SetActive(false);
+
+        // 해당 장비 활성화
+        switch (equipType)
+        {
+            case EquipmentType.Sword:
+                Sword.SetActive(true);
+                Shield.SetActive(true);
+                break;
+            case EquipmentType.Pickaxe:
+                Pickaxe.SetActive(true);
+                break;
+            case EquipmentType.Bow:
+                Bow.SetActive(true);
+                break;
+            case EquipmentType.Magic:
+                Magic.SetActive(true);
+                break;
+        }
+
+        // 애니메이터 레이어 활성화
+        ActivateAnimationLayer(equipType.ToString());
+    }
+
+    private void ActivateAnimationLayer(string layerName)
+    {
+        for (int i = 0; i < animator.layerCount; i++)
+        {
+            string currentLayerName = animator.GetLayerName(i);
+            animator.SetLayerWeight(i, currentLayerName == layerName ? 1f : 0f);
+        }
+    }
+}
