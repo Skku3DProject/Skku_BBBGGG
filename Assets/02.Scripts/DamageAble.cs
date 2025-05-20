759ac61289f0b6fe50ebdc29bffc696a7f9bca15
ParkSoonHong <95293014+ParkSoonHong@users.noreply.github.com>
2025-05-20 09:33:51 +0900

    [5 -20] 박순홍 - DamageAble 수정

--- Assets/02.Scripts/DamageAble.cs
+++ Assets/02.Scripts/DamageAble.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public interface DamageAble
 {
-    void TakeDamage(float damage);
+    void TakeDamage(Damage damage);
 
+}
+
+public class Damage
+{
+    public float Value;
+    public GameObject from;
+    public float KnockbackPower;
+ 
+    public Damage(float value, GameObject from, float knockbackPower = 0)
+    {
+        Value = value;
+        this.from = from;
+        KnockbackPower = knockbackPower;
+    }
 }
