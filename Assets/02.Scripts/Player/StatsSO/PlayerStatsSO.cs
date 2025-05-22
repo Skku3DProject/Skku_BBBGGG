41f89d7ce1c458e9ce0fa505aaab9f79726ccdbf
minju8843 <minju8843@naver.com>
2025-05-22 16:51:36 +0900

    [05-22] 서민주 - 스테미너, 경험치, 레벨추가

--- Assets/02.Scripts/Player/StatsSO/PlayerStatsSO.cs
+++ Assets/02.Scripts/Player/StatsSO/PlayerStatsSO.cs
@@ -6,4 +6,8 @@ public class PlayerStatsSO : ScriptableObject
     public float MaxHealth;
     public float MoveSpeed;
     public float JumpPower;
+    public float Stamina;
+    public float Exp;
+    public int Level;
+
 }
